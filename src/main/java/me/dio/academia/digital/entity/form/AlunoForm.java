8438f7b47fcb82532c5eb4e187f34fe1d67357ff
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-08-01 17:48:01 -0300

    trabalhando as entidades

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -1,7 +1,13 @@
 package me.dio.academia.digital.entity.form;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AlunoForm {
 
   private String nome;
